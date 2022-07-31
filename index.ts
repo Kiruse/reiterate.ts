30cb831cbeefc43c27066235b898f866db3bcbc7
kiruse <26940742+Kiruse@users.noreply.github.com>
2022-07-31 14:35:54 +0200

    add each, filter, map utils & docstrings

--- index.ts
+++ index.ts
@@ -1,14 +1,44 @@
 
 export type Iterthing<T> = Iterable<T> | Iterator<T>;
 
+/** Create an iterator from the given `Iterthing`. */
 //@ts-ignore
 export const iterate = <T>(x: Iterthing<T>): Iterator<T> => Symbol.iterator in x ? x[Symbol.iterator]() : x;
 
+/** Create a one-time use iterable from the given `Iterthing`. */
 //@ts-ignore
 export const iterable = <T>(x: Iterthing<T>): Iterable<T> => Symbol.iterator in x ? x : {[Symbol.iterator]: () => x};
 
+/** Collect all items yielded by `it` into a reusable array. */
 export const collect = <T>(it: Iterthing<T>) => [...iterable(it)];
 
+/** Call `callback` for every item yielded by `it`. */
+export function* each<T>(it: Iterthing<T>, callback: (item: T) => void) {
+  for (const item of iterable(it))
+    callback(item);
+}
+
+/** Filter items yielded by `it` by `callback`. If `callback` returns
+ * `true` for an item, this item will be yielded, otherwise skipped.
+ */
+export function* filter<T>(it: Iterthing<T>, callback: (item: T) => boolean) {
+  for (const item of iterable(it)) {
+    if (callback(item))
+      yield item;
+  }
+}
+
+/** Map items yielded by `it` using `callback`. The transformed items are yielded. */
+export function* map<I, O>(it: Iterthing<I>, callback: (item: I) => O) {
+  for (const item of iterable(it)) {
+    yield callback(item);
+  }
+}
+
+/** "Zip" two `Iterthing`s together, yielding pairs of `[T1, T2]`. If
+ * either `lhs` or `rhs` terminate early, their respective value within
+ * future pairs will simply be `undefined`.
+ */
 export function* zip<T1, T2>(lhs: Iterthing<T1>, rhs: Iterthing<T2>) {
   const lit = iterate(lhs);
   const rit = iterate(rhs);
